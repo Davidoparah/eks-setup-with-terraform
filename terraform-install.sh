e147fd12da63f75e168337ca2cc83cf6dd97d868
acadalearning <108498076+acadalearning@users.noreply.github.com>
2022-11-12 11:18:19 -0700

    Update terraform-install.sh

--- terraform-install.sh
+++ terraform-install.sh
@@ -6,7 +6,7 @@ sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(l
 sudo apt-get update && sudo apt-get install terraform
 terraform -version
 
-OR
+# OR
 
 # sudo apt install wget unzip -y
 # wget https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
