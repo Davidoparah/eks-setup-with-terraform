86ebca2ef8fe3608dc3d4fa7ed3065e14b7b30fe
Davidoparah <107112435+Davidoparah@users.noreply.github.com>
2023-08-23 04:15:31 +0100

    Update terraform-install.sh

--- terraform-install.sh
+++ terraform-install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # install terraform in ubuntu server
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+#sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt install  software-properties-common gnupg2 curl
+curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
+sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt install terraform
 terraform -version
 
 # OR
